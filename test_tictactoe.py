20e8e1756cd6d90fe76a37ffcf87730b83b0ad92
jack-diamond <jhdiamond96@gmail.com>
2020-02-11 16:46:12 -0800

    adding unit tests

--- test_tictactoe.py
+++ test_tictactoe.py
@@ -3,38 +3,57 @@ from board import Board
 from game import Game
 from player import Player
 
-class TestMethods(unittest.TestCase):
+class TestMarkSquare(unittest.TestCase):
 
-    def test_mark_square(self):
+    def test1(self):
+        '''
+        Test on empty square.
+        '''
         board = Board()
-        board.mark_square(2, 0, 'x')
-        self.assertEqual(board.board[0][2], 'x')
+        board.mark_square(0, 2, 1)
+        self.assertEqual(board.board[0][2], 1)
 
-    def test_row_last_complete(self):
+    def test2(self):
+        '''
+        Test on non-empty square.
+        '''
         board = Board()
-        board.mark_square(0, 2, 'x')
-        board.mark_square(1, 2, 'x')
-        board.mark_square(2, 2, 'x')
-        board.has_winner
+        board.mark_square(0, 2, 1)
+        board.mark_square(0, 2, -1)
+        self.assertEqual(board.board[0][2], 1)
+
+class TestHasWinner(unittest.TestCase):
+
+    def test1(self):
+        '''
+        Test on diagonal winner.
+        '''
+        board = Board()
+        board.mark_square(0, 0, 1)
+        board.mark_square(1, 1, 1)
+        board.mark_square(2, 2, 1)
         self.assertEqual(board.has_winner(), "x")
 
-    def test_row_first_complete(self):
+    def test2(self):
+        '''
+        Test on other diagonal winner.
+        '''
         board = Board()
-        board.mark_square(0, 0, 'o')
-        board.mark_square(1, 0, 'o')
-        board.mark_square(2, 0, 'o')
-        board.has_winner
-        self.assertEqual(board.has_winner, "o")
+        board.mark_square(2, 0, 1)
+        board.mark_square(1, 1, 1)
+        board.mark_square(0, 2, 1)
+        self.assertEqual(board.has_winner(), "x")
 
-
-    def test_no_winner(self):
+    def test3(self):
+        '''
+        Test no winner with some squares marked.
+        '''
         board = Board()
-        board.mark_square(0, 2, 'x')
-        board.mark_square(1, 2, 'o')
-        board.mark_square(2, 2, 'x')
+        board.mark_square(0, 2, 1)
+        board.mark_square(1, 2, -1)
+        board.mark_square(2, 2, 1)
         board.has_winner
-        self.assertEqual(board.has_winner, "n")
-
+        self.assertEqual(board.has_winner(), ".")
 
 if __name__ == '__main__':
     unittest.main()
